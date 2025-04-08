808034b5c093c1997b4a65907c971e1fcad9ab3c
LockeLamora1 <contealexandre3@proton.me>
2025-04-08 11:24:26 +0200

    modification of V_Recette.php and V_ListeRecettes.php

--- Vues/Recettes/V_ListeRecettes.php
+++ Vues/Recettes/V_ListeRecettes.php
@@ -42,7 +42,7 @@
     <!-- fin menu -->
 
     <div
-      class="flex flex-col justify-center items-center w-[95%]  pb-110 mx-auto border-2 border-solid rounded-sm p-8 mt-7"
+      class="flex flex-col justify-center items-center w-[95%] pb-110 mx-auto border-2 border-solid rounded-sm p-8 mt-7"
     >
       <h1 class="text-3xl font-bold text-center mb-4 mt-8">Nos recettes</h1>
 
@@ -66,50 +66,21 @@
             </tr>
           </thead>
           <tbody class="divide-y divide-gray-200">
-            <tr>
-              <td class="px-6 py-4 border-r-2 border-solid border-t-2">img</td>
-              <td class="px-6 py-4 border-r-2 border-solid border-t-2">
-                libelleType
-              </td>
-              <td class="px-6 py-4 border-r-2 border-solid border-t-2">
-                nom de la recette
-              </td>
-              <td class="border-solid border-t-2">
+            <?php
+            foreach (listeRecettes as $uneRecette){
+              echo "<tr>";
+                echo "<td class='px-6 py-4 border-r-2 border-solid border-t-2'>". $uneRecette->getImage()."</td>";
+                echo "<td class='px-6 py-4 border-r-2 border-solid border-t-2'>". $uneRecette->getLibelle()."</td>";
+                echo "<td class='px-6 py-4 border-r-2 border-solid border-t-2'>". $uneRecette->getDescription()."</td>";
+                echo "<td class='border-solid border-t-2'>
                 <button
-                  class="bg-amber-100 text-black font-bold py-2 px-4 rounded-full"
+                  class='bg-amber-100 text-black font-bold py-2 px-4 rounded-full'
                 >
                   Voir la recette
                 </button>
-              </td>
-            </tr>
-            <tr>
-              <td class="px-6 py-4 border-r-2 border-solid">img</td>
-              <td class="px-6 py-4 border-r-2 border-solid">libelleType</td>
-              <td class="px-6 py-4 border-r-2 border-solid">
-                nom de la recette
-              </td>
-              <td>
-                <button
-                  class="bg-amber-100 text-black font-bold py-2 px-4 rounded-full"
-                >
-                  Voir la recette
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td class="px-6 py-4 border-r-2 border-solid">img</td>
-              <td class="px-6 py-4 border-r-2 border-solid">libelleType</td>
-              <td class="px-6 py-4 border-r-2 border-solid">
-                nom de la recette
-              </td>
-              <td>
-                <button
-                  class="bg-amber-100 text-black font-bold py-2 px-4 rounded-full"
-                >
-                  Voir la recette
-                </button>
-              </td>
-            </tr>
+              </td>";
+            };
+            ?>
           </tbody>
         </table>
       </div>
