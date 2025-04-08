74f38039b02c8be2a8ae3923b028379dc111ccda
Galissaires <mathias.galissaires1@gmail.com>
2025-04-08 11:33:24 +0100

    href of index + Fixe Controlleurs

--- Controleurs/C_Recettes.php
+++ Controleurs/C_Recettes.php
@@ -1,24 +1,29 @@
 <?php
-include("./Modeles/DAO/DAO_Recettes");
+include("./Modeles/DAO/DAO_Recettes.php");
 
 if (isset($_GET['action']))
-    $action = filter_var($_GET['action'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  $action = filter_var($_GET['action'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 else
-    $action = "Recettes";
-  
+  $action = "ListeRecettes";
+
 switch ($action) {
 
-  case 'Recettes':
-      $sourceDeDonnees = new DAO_Recettes();
-      $listeRecettes = $sourceDeDonnees->getLesRecettes();
+  case 'ListeRecettes':
+    $sourceDeDonnees = new RecettesDAO();
+    $listeRecettes = $sourceDeDonnees->getLesRecettes();
 
-      include("./vues/Recettes/V_ListeRecettes.php");
-  break;
+    include("./vues/Recettes/V_ListeRecettes.php");
+    break;
   case 'Recette':
-    $sourceDeDonnees = new DAO_Recettes();
+    $sourceDeDonnees = new RecettesDAO();
     $listeRecettes = $sourceDeDonnees->recette();
 
     include("./vues/Recettes/V_Recette.php");
-  break;
-  
+    break;
+  default:
+    $sourceDeDonnees = new RecettesDAO();
+    $listeRecettes = $sourceDeDonnees->getLesRecettes();
+
+    include("./vues/Recettes/V_ListeRecettes.php");
+    break;
 }
