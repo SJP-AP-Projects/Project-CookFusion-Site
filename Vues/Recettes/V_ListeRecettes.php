b16112a016862110ad6a5d421b2cb0c1b0895973
LockeLamora1 <contealexandre3@proton.me>
2025-04-09 14:28:26 +0200

    fix : delete double menu

--- Vues/Recettes/V_ListeRecettes.php
+++ Vues/Recettes/V_ListeRecettes.php
@@ -7,37 +7,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
   </head>
   <body>
-    <!-- menu -->
-    <header class="flex items-center justify-between px-4 py-2 mr-8 ml-8">
-      <!-- Logo -->
-      <div
-        class="bg-orange-200 rounded-lg shadow w-[120px] h-[50px] overflow-hidden"
-      >
-        <img
-          src="../logo.png"
-          alt="Logo Rivière Gourmande"
-          class="w-full h-full object-cover"
-        />
-      </div>
 
-      <!-- Menu -->
-
-      <nav
-        class="flex space-x-10 bg-white py-2 px-6 rounded-lg shadow-md h-[50px]"
-      >
-        <a href="#" class="text-gray-800 hover:underline">Accueil</a>
-        <a href="#" class="text-gray-800 hover:underline">Nos sessions</a>
-        <a href="#" class="font-bold underline text-black">Nos recettes</a>
-        <a href="#" class="text-gray-800 hover:underline">Mes reservations</a>
-      </nav>
-
-      <!-- User -->
-      <div
-        class="bg-white px-4 py-2 rounded-lg shadow-md font-semibold text-sm h-[50px]"
-      >
-        User_01
-      </div>
-    </header>
 
     <!-- fin menu -->
 
